8c86389a4fe01da292c82769026e2c6f89687bc7
Edwin Roman <pazamoryperdon@gmail.com>
2021-08-07 17:22:50 -0500

    feat(web):add show selected project

--- cliente/src/components/tareas/ListadoTareas.js
+++ cliente/src/components/tareas/ListadoTareas.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Tarea from './Tarea'
+import proyectoContext from '../../context/proyectos/proyectoContext'
 
-const tareasProyecto = [
-    {nombre:'Elegir Plataforma', estado:true},
-    {nombre:'Elegir dominio', estado:false},
-    {nombre:'Elegir hosting', estado:true},
-    {nombre:'Diseñar interfaz', estado:false},
 
-]
 
 const ListadoTareas = () => {
+
+    //extraer proyectos del stateInitial
+    const proyectosContext = useContext(proyectoContext)
+    const {proyecto} = proyectosContext
+
+    //si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>
+
+    const [proyectoActual] = proyecto
+
+    const tareasProyecto = [
+        {nombre:'Elegir Plataforma', estado:true},
+        {nombre:'Elegir dominio', estado:false},
+        {nombre:'Elegir hosting', estado:true},
+        {nombre:'Diseñar interfaz', estado:false},
+    
+    ]
     return (
         <>
-            <h2>Proyecto : Tienda virtual</h2>
+            <h2>Proyecto : {proyectoActual.nombre}</h2>
             <ul className='listado-tareas'>
                 {tareasProyecto.length === 0
                     ?(<li>No hay tareas</li>)
